337e94f12ab4b24275cca59f013d93bc4e612faa
Aditya Kumar <git@adityais.dev>
2025-01-31 09:06:12 +0000

    Update documentation links to reflect crate name change to "pastey"

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-//! [![github]](https://github.com/dtolnay/paste)&ensp;[![crates-io]](https://crates.io/crates/paste)&ensp;[![docs-rs]](https://docs.rs/paste)
+//! [![github]](https://github.com/as1100k/pastey)&ensp;[![crates-io]](https://crates.io/crates/pastey)&ensp;[![docs-rs]](https://docs.rs/pastey)
 //!
 //! [github]: https://img.shields.io/badge/github-8da0cb?style=for-the-badge&labelColor=555555&logo=github
 //! [crates-io]: https://img.shields.io/badge/crates.io-fc8d62?style=for-the-badge&labelColor=555555&logo=rust
